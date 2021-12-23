baa1a911eb096cb515997dcfa6fc800bf38436ef
Meihao-Han <meihao.hsu@gmail.com>
2021-12-23 16:53:43 +0800

    更換照片

--- resources/views/admin/component/sidebar-menu.blade.php
+++ resources/views/admin/component/sidebar-menu.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('admin.index')}}" style="height:56px;">
-        <img src="/storage/image/meihao-LOGO.png" alt="meihao Logo" class="brand-image elevation-1" style="width: 100%;padding: 10px">
+        <img src="/storage/image/LOGO.png" alt="meihao Logo" class="brand-image elevation-1" style="width: 100%;padding: 10px">
         <span class="brand-text font-weight-light"></span>
     </a>
 
