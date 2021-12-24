9d9ab2780ce5f2b5cf53593e9cae4a88d261a76e
Han <sir3453715@gmail.com>
2021-12-24 14:56:19 +0800

    增加網站操作歷史紀錄相關Controller

---
+++ app/Http/Controllers/Admin/Menu/ActionLogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Menu;
+
+use App\Http\Controllers\Controller;
+use App\Models\ActionLog;
+use Illuminate\Http\Request;
+
+class ActionLogController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $action_log = ActionLog::orderBy('created_at','desc')->paginate(25);
+
+        return view('admin.setting.ActionLog',[
+            'action_log'=>$action_log,
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Admin/Menu/LoginLogController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Menu;
+
+use App\Http\Controllers\Controller;
+use App\Models\LoginLog;
+use Illuminate\Http\Request;
+
+class LoginLogController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $login_log = LoginLog::orderBy('created_at','desc')->paginate(25);
+        return view('admin.setting.LoginLog',[
+            'login_log'=>$login_log,
+        ]);
+    }
+}
