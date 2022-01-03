3896e61b607d29d928023ed9559d579991d287d9
Han <sir3453715@gmail.com>
2022-01-03 16:28:06 +0800

    訂單明細Controller 編輯頁面function 儲存訂單資料 function

--- app/Http/Controllers/Admin/Menu/OrderDetailController.php
+++ app/Http/Controllers/Admin/Menu/OrderDetailController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Admin\Menu;
 
 use App\Http\Controllers\Controller;
+use App\Models\ActionLog;
 use App\Models\Country;
 use App\Models\Order;
+use App\Models\OrderBox;
+use App\Models\OrderBoxItem;
 use App\Models\SailingSchedule;
 use Facebook\Facebook;
 use Facebook\FacebookRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class OrderDetailController extends Controller
 {
@@ -75,6 +79,10 @@ class OrderDetailController extends Controller
      */
     public function edit($id)
     {
+        $order = Order::find($id);
+        return view('admin.orderBoxes.editOrderDetail',[
+            'order'=>$order,
+        ]);
 
     }
 
@@ -88,6 +96,74 @@ class OrderDetailController extends Controller
     public function update(Request $request, $id)
     {
 
+        /* 修改訂單資料 */
+        $order = Order::find($id);
+        $data = [
+            'status' => $request->get('status'),
+            'pay_status' => $request->get('pay_status'),
+            'total_price' => $request->get('total_price'),
+            'shipment_use' => $request->get('shipment_use'),
+            'sender_name' => $request->get('sender_name'),
+            'sender_phone' => $request->get('sender_phone'),
+            'sender_address' => $request->get('sender_address'),
+            'sender_company' => $request->get('sender_company'),
+            'sender_taxid' => $request->get('sender_taxid'),
+            'sender_email' => $request->get('sender_email'),
+            'for_name' => $request->get('for_name'),
+            'for_phone' => $request->get('for_phone'),
+            'for_address' => $request->get('for_address'),
+            'for_company' => $request->get('for_company'),
+            'for_taxid' => $request->get('for_taxid'),
+            'invoice' => $request->get('invoice')
+        ];
+        $order->fill($data);
+        ActionLog::create_log($order);
+        $order->save();
+
+        /* 刪除舊箱子資料 */
+        $oldBox = OrderBox::where('order_id',$id);
+        $oldBoxItem = OrderBoxItem::where('order_id',$id);
+        if($oldBoxItem)
+            $oldBoxItem->delete();
+
+        if($oldBox)
+            $oldBox->delete();
+
+        /* 儲存新箱子資料 */
+        $start_item = 0;
+        $total_items_num = 0;
+        $boxSize = sizeof($request->get('box_weight'));
+        for($i = 0; $i<$boxSize; $i++){
+            $boxData = [
+                'order_id'=>$order->id,
+                'box_seccode'=>$order->serial_number.'-'.$order->person_number.'-'.($i+1),
+                'box_weight'=>$request->get('box_weight')[$i],
+                'box_length'=>$request->get('box_length')[$i],
+                'box_width'=>$request->get('box_width')[$i],
+                'box_height'=>$request->get('box_height')[$i],
+                'box_price'=>$request->get('box_price')[$i],
+                'tracking_number'=>$request->get('tracking_number')[$i],
+            ];
+            $box = OrderBox::create($boxData);
+            ActionLog::create_log($box,'create');
+
+            $itemSize = $request->get('box_items_num')[$i];
+            $total_items_num = $total_items_num + $itemSize;
+            for($j = $start_item;$j<$total_items_num;$j++){
+                $itemData = [
+                    'order_id'=>$order->id,
+                    'box_id'=>$box->id,
+                    'item_name'=>$request->get('item_name')[$j],
+                    'item_num'=>$request->get('item_num')[$j],
+                    'unit_price'=>$request->get('unit_price')[$j],
+                ];
+                $item = OrderBoxItem::create($itemData);
+                ActionLog::create_log($item,'create');
+                $start_item++;
+            }
+        }
+
+        return redirect(route('admin.order-detail.index'))->with('message', '訂單 '.$order->seccode.'已完成修改');
     }
 
     /**
