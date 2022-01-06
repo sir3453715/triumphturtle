609c7b0fd4e47fc073aec9ade879f1ea18735e28
Han <sir3453715@gmail.com>
2022-01-06 16:14:07 +0800

    修改前台 Controller 與 Route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('index');
-Route::post('/punch', 'HomeController@punch')->name('punch');
 
 
 
