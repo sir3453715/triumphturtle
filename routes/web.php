c2272f8f708bcb7a07f59d4c290246d70e0bac72
ko <>
2022-01-18 20:11:59 +0800

    刪掉contact page 加入about page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('index');
-Route::get('/contact', 'HomeController@contact')->name('contact');
+Route::get('/about', 'HomeController@about')->name('about');
 
 
 /**
