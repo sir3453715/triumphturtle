6516ad2895fe0102fa906924f243d816883278d2
Han <sir3453715@gmail.com>
2022-02-14 14:37:59 +0800

    增加網站一般設定

--- config/options.php
+++ config/options.php
@@ -5,15 +5,35 @@ return [
     // 設定的項目
     'fields' => [
         'site_name' => [
-            'label'     => 'siteName',
+            'label'     => '網站名稱',
             'validator' => 'required',
             'required'  => true
         ],
         'site_description' => [
-            'label' => 'siteDescription'
+            'label' => '網站簡介'
         ],
-        'service' => [
-            'label' => 'service',
+        'company_address' => [
+            'label' => '公司地址'
+        ],
+        'company_tel' => [
+            'label' => '公司電話'
+        ],
+        'company_email' => [
+            'label' => '公司信箱'
+        ],
+        'facebook' => [
+            'label' => 'FB連結'
+        ],
+        'line' => [
+            'label' => 'Line連結'
+        ],
+        'faq' => [
+            'label' => '常見問題',
+            'type' => 'editor',
+            'weight' => '300',
+        ],
+        'send_content' => [
+            'label' => '寄送限制注意事項',
             'type' => 'editor',
             'weight' => '300',
         ],
