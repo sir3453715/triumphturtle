a2958b0d80a3cfa235a0664b20d72ebb2a384daa
Han <sir3453715@gmail.com>
2022-02-14 14:39:21 +0800

    修改後台介面

--- resources/views/admin/component/footer.blade.php
+++ resources/views/admin/component/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021 Meihao.</strong>All rights reserved.
+    <strong>Copyright &copy; 2021 Nomadot.RHan </strong>All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <small>
             Template : <a href="https://adminlte.io">AdminLTE.io</a> <b>Version</b> 3.1.0
