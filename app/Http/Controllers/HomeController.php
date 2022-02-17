c3695a2d985790100ae9dcbb2d9a9bbf3a13ca41
Han <sir3453715@gmail.com>
2022-02-17 16:43:08 +0800

    動態產生訂單修改驗證碼Ajax

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendMailQueueJob;
 use App\Models\Country;
+use App\Models\Order;
 use App\Models\SailingSchedule;
 use App\Models\Warehouse;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
 class HomeController extends Controller
@@ -70,9 +73,28 @@ class HomeController extends Controller
         $warehouses = Warehouse::all();
         return view('location',['warehouses'=>$warehouses]);
     }
-    public function tracking()
+    public function tracking(Request $request)
     {
-        return view('tracking');
+        $queried=['keyword'=>''];
+        if($request->get('keyword')) {
+            $queried['keyword'] = $request->get('keyword');
+            $sailing_ids = SailingSchedule::where('status','1')->pluck('id');
+            $orders = Order::whereIn('sailing_id',$sailing_ids)->where(function ($query) use ($queried){
+                $query->orwhere('seccode',$queried['keyword']);
+                $query->orwhere('sender_phone',$queried['keyword']);
+            })->get();
+        }
+        return view('tracking', [
+            'orders'=>$orders??null,
+            'queried'=>$queried
+        ]);
+    }
+    public function trackingCaptcha($seccode)
+    {
+        $order = Order::where('seccode',$seccode)->first();
+        return view('trackingCaptcha',[
+            'order'=>$order
+        ]);
     }
     public function editSuccess()
     {
@@ -102,6 +124,28 @@ class HomeController extends Controller
             $sailings = $sailings->paginate(6);
             return view('component.ajaxSailingData', ['sailings' => $sailings])->render();
         }
+    }
+    public function updateToken(Request $request){
+        $return = false;
+        $order = Order::find($request->get('order_id'));
+        if ($order){
+
+            $updateToken=substr(md5(uniqid(rand())),0,8);  ##產生隨機字串
+            $updateToken = preg_replace('/\[O|0|I|i|L\]/',rand(2,9),$updateToken);  #排除掉特定字元
+            $order->fill(['updateToken'=>$updateToken]);
+            $order->save();
+            $data = [
+                'email'=>$order->sender_email,
+                'subject'=>'修改訂單驗證碼通知信',
+                'for_title'=>$order->sender_name,
+                'msg'=>'您的訂單修改驗證碼為:'.$updateToken.'請再透過網站進行驗證後修改訂單，<br/>'.
+                    '或透過下方連結進行驗證手續<a target="_blank" href="'.route('tracking-captcha',['seccode'=>$order->seccode]).'"></a>',
+            ];
+            dispatch(new SendMailQueueJob($data));
+
+            $return = true;
+        }
+        return json_encode($return); //json
     }
 
 }
