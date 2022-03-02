2b6c07b0b4b957ef20ff746d01ad3685d45af36c
ko <>
2022-03-03 02:20:26 +0800

    裝箱單切版

---
+++ resources/views/package.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width">
+    <title>海龜集運裝箱單</title>
+    <link href="" rel="stylesheet">
+    <style>
+        table {
+            width: 95%;
+            margin: auto;
+            border: none;
+            border-collapse: collapse;
+            font-size: 20px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        td {
+            border: 1px solid #000;
+            padding: 5px 10px;
+        }
+        .center-text{
+            text-align: center;
+        }
+        .export-header{
+            background: #B6B5B5;
+            font-weight: bold;
+        }
+
+        .font-title {
+font-weight: bold;
+        }
+
+        .bg-green {
+            background-color: rgb(149, 215, 193);
+        }
+
+        .main-sender {
+            height: 100px;
+        }
+
+    </style>
+</head>
+<body>
+    <table>
+        <tr class="main-sender">
+            <td colspan="1" class="font-title">NAME /ADDRESS /TEL</td>
+            <td colspan="7">CHEN, YEN-HUA / ul. Polnocna 6/43, 20-064, Lublin, Poland/ 572633167</td>
+        </tr>
+        <tr>
+            <td colspan="8" class="center-text font-title export-header">PACKING/WEIGHT LIST</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">PACKING LIST of</td>
+            <td colspan="7"></td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">For account and risk of Messrs</td>
+            <td colspan="7">陳燕樺</td>
+        </tr> 
+        <tr>
+            <td colspan="1" class="font-title">Address</td>
+            <td colspan="7">806 高雄市前鎮區民權二路522路20號</td>
+        </tr> <tr>
+            <td colspan="1" class="font-title">TEL</td>
+            <td colspan="7">0922998357</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">From</td>
+            <td colspan="3">Poland</td>
+            <td colspan="1" class="font-title">TO</td>
+            <td colspan="3">Taiwan</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">purpose</td>
+            <td colspan="7">行李後送</td>
+        </tr> 
+        <tr class="export-header center-text">
+            <td colspan="8">list detail</td>
+        </tr>
+        <tr class="font-title center-text">
+            <td>Packing No.</td>
+            <td>Description</td>
+            <td>unit price</td>
+            <td>Quantity</td>
+            <td>Net Weight</td>
+            <td>Gross Weight</td>
+            <td>Measurement(CM)</td>
+            <td>Value (USD)</td>
+        </tr>
+        <tr class="center-text">
+            <td rowspan="2" class="bg-green">TS211001002-1 -1</td>
+            <td>book</td>
+            <td>10</td>
+            <td>20</td>
+            <td>19KG</td>
+            <td>20KG</td>
+            <td>63*31*33</td>
+            <td rowspan="2">210</td>
+        </tr>
+        <tr class="center-text">
+            <td>shoes</td>
+            <td>10</td>
+            <td>1</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
+
