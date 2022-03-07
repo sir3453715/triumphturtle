ca3a53315523711b97bb9f7b114ee8731eb9e3d5
ko <>
2022-03-07 20:09:57 +0800

    請款單切版

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width">
+    <title>海龜集運請款單</title>
+    <link href="" rel="stylesheet">
+    <style>
+        table {
+            width: 95%;
+            margin: auto;
+            border: none;
+            border-collapse: collapse;
+            font-size: 20px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        td {
+            border: 1px solid #000;
+            padding: 5px 10px;
+        }
+        .center-text{
+            text-align: center;
+        }
+        .export-header{
+            background: #B6B5B5;
+            font-weight: bold;
+        }
+
+        .font-title {
+font-weight: bold;
+        }
+
+        .bg-green {
+            background-color: rgb(149, 215, 193);
+        }
+
+        .bg-gray {
+            background-color: rgb(233, 233, 233);
+        }
+
+        .main-sender {
+            height: 100px;
+        }
+
+    </style>
+</head>
+<body>
+    <table>
+        <tr class="main-sender">
+            <td colspan="2" rowspan="5" class="font-title"><img width="300px" src="/storage/image/triumphturtle.jpg"></td>
+            <td colspan="4" class="font-title" style="font-size: 28px;">凱漩國際有限公司</td>
+        </tr>
+        <tr>
+            <td colspan="4">TRIUMPH INTERNATIONAL TRADING CO.,  LTD</td>
+        </tr>
+        <tr>
+            <td colspan="4">2F, No. 113, Chenggong Rd., Sanchong Dist., 
+                New Taipei City 241012, Taiwan (R.O.C.)</td>
+        </tr> 
+        <tr>
+            <td colspan="4">+886 2 29780058</td>
+        </tr>
+        <tr>
+            <td colspan="4">www.triumphturtle.com</td>
+        </tr>
+        <tr class="export-header center-text">
+            <td colspan="6">BILL TO</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">Company: </td>
+            <td colspan="2">XXX公司</td>
+            <td colspan="1" class="font-title">Order No.</td>
+            <td colspan="2">T202203414-1</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">Tax ID:</td>
+            <td colspan="2">98897779</td>
+            <td colspan="1" class="font-title">Payment Due</td>
+            <td colspan="2">2022/2/28</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">Contact:</td>
+            <td colspan="5">Wen Chang</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">Phone:</td>
+            <td colspan="5">Leolin@breiten.biz</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">Email:</td>
+            <td colspan="5">98897779</td>
+        </tr>
+        <tr class="export-header center-text">
+            <td colspan="6">Payment detail</td>
+        </tr>
+        <tr class="font-title center-text">
+            <td>Product</td>
+            <td>Quantity</td>
+            <td>Unit</td>
+            <td>Country</td>
+            <td>Unit Price</td>
+            <td>Total Price</td>
+        </tr>
+        <tr class="center-text">
+            <td> Delivery Fee</td>
+            <td>3</td>
+            <td>box</td>
+            <td>波蘭</td>
+            <td>NT$2500</td>
+            <td>NT$7500</td>
+        </tr>
+        <tr class="center-text">
+            <td colspan="4"></td>
+            <td colspan="1" class="bg-gray font-title ">Subtotal:</td>
+            <td colspan="1" class="bg-gray">NT$8500</td>
+        </tr>
+        <tr class="center-text">
+            <td colspan="4"></td>
+            <td colspan="1" class="bg-gray font-title ">營業稅5%:</td>
+            <td colspan="1" class="bg-gray">NT$425</td>
+        </tr>
+        <tr class="center-text">
+            <td colspan="4"></td>
+            <td colspan="1" class="bg-green font-title ">Total:</td>
+            <td colspan="1" class="bg-green">NT$8925</td>
+        </tr>
+        <tr class="export-header center-text">
+            <td colspan="6">匯款資訊</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">銀行:</td>
+            <td colspan="5">國泰世華銀行 013</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">帳號:</td>
+            <td colspan="5">220-03-500712-1</td>
+        </tr>
+        <tr>
+            <td colspan="1" class="font-title">戶名:</td>
+            <td colspan="5">凱漩國際有限公司</td>
+        </tr>
+    </table>
+</body>
+</html>
+
