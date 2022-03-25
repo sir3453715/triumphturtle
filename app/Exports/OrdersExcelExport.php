6d1865b4706dad571020c044003268f126252045
Han <sir3453715@gmail.com>
2022-03-25 15:02:08 +0800

    增加訂單匯出excel Export

---
+++ app/Exports/OrdersExcelExport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class OrdersExcelExport implements FromView
+{
+    public $data;
+
+    public function __construct(array $data)
+    {
+        $this->data   = $data;
+    }
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function view(): View
+    {
+        return view('pdf.default-package');
+//        return view('pdf.package',[
+//            'data'=>$this->data,
+//        ]);
+    }
+
+    public function title(): string
+    {
+        // 設定工作䈬的名稱
+        return '';
+    }
+}
