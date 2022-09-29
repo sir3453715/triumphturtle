db684f2f4a93cca4f698967875edea0ee70c285b
Han <sir3453715@gmail.com>
2022-09-29 10:20:16 +0800

    增加訂單寄回欄位 & 補上發票時間

---
+++ database/migrations/2022_09_29_092727_add_order_reback_field.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderRebackField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('reback')->comment('是否寄回台灣[1:是,2:否]')->nullable();
+            $table->string('reback_time')->comment('寄回時間')->nullable();
+            $table->string('invoice_time')->comment('發票時間')->nullable();//補發票時間
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('reback');
+            $table->dropColumn('reback_time');
+            $table->dropColumn('invoice_time');
+        });
+    }
+}
